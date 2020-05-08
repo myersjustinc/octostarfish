945ac081e2b3a6a98cedd0e07e56e1fd2ce14e70
Justin Myers <justin@justinmyers.net>
2020-05-08 18:38:37 -0500

    Pass arguments more explicitly

--- octostarfish/octostarfish.py
+++ octostarfish/octostarfish.py
@@ -1,13 +1,27 @@
+import sys
+
+
 class Octostarfish(object):
     """Expose all public functionality.
 
     Use `Octostarfish.run()` to kick things off!
     """
     @classmethod
-    def run(cls):
+    def run(cls, user, token):
         """Run the Octostarfish job."""
-        fish = cls()
+        fish = cls(user, token)
         # TODO: Keep going.
 
-    def __init__(self):
-        pass
+    def __init__(self, user, token):
+        if user is None:
+            sys.stderr.write(
+                'You must specify a user with --user or the GITHUB_USER '
+                'environment variable.\n')
+            sys.exit(1)
+        if token is None:
+            sys.stderr.write(
+                'You must specify a token with --token or the '
+                'GITHUB_API_TOKEN environment variable.\n')
+            sys.exit(1)
+        self.user = user
+        self.token = token
--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+import argparse
+import os
+
 from octostarfish import Octostarfish
 
-Octostarfish.run()
+
+parser = argparse.ArgumentParser(description=(
+    'Clone GitHub repositories that have been starred by a given user.'))
+parser.add_argument(
+    '-u', '--user', default=os.getenv('GITHUB_USER'),
+    help='GitHub user whose stars should be cloned')
+parser.add_argument(
+    '-t', '--token', default=os.getenv('GITHUB_API_TOKEN'),
+    help='GitHub API token')
+args = parser.parse_args()
+
+
+Octostarfish.run(args.user, args.token)
